5f1ffd2cf53d33193ce815187be889f220251bed
Pablo Portillo <paabloportillo@gmail.com>
2024-04-05 18:22:43 +0200

    ft_lstclear

--- Libft/ft_lstclear.c
+++ Libft/ft_lstclear.c
@@ -1,20 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pportill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/05 13:56:21 by pportill          #+#    #+#             */
-/*   Updated: 2024/04/05 13:56:21 by pportill         ###   ########.fr       */
+/*   Created: 2024/04/05 17:59:40 by pportill          #+#    #+#             */
+/*   Updated: 2024/04/05 17:59:40 by pportill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-void	ft_lstdelone(t_list *lst, void (*del)(void *))
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+
+void    ft_lstclear(t_list **lst, void (*del)(void *))
 {
-	del(lst->content);
-	free(lst);
+    t_list  *aux;
+
+    aux = lst;
+    while(aux)
+    {
+        ft_lstdelone(*lst,del);
+        aux = aux->next;
+        lst = aux;
+    }
+    free(*lst);
+    *lst= NULL;
 }
